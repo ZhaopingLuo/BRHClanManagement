e899160bd1d656a48b2ab105c91d24707732edb8
xmluozp <xmluozp@gmail.com>
2018-07-15 03:45:43 -0500

    ignore

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:31337/yyyclan';
+$config['base_url'] = 'http://localhost:31337/yyyclan ';
 
 /*
 |--------------------------------------------------------------------------
